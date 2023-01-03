560981f9a1e044b11e92b2f22182ad9c0b41a6f9
Moses <mosganda2022@gmail.com>
2023-01-03 08:55:48 -0800

    modify: about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,25 +3,25 @@ import React from 'react'
 
 function About() {
     return (
-        <div style={{backgroundColor:"white", textAlign:"center", padding:"20px", maxWidth:"100%", margin:"20px 10px"}}>
+        <div className='about-page'>
             <h3>About Us</h3>
-            <p>Mosganda is an online platform and a marketplace for buying and selling of goods and services, located in Warri, Delta State, Nigeria. Mosganda provides the platform for entrepreneurs to bring their business online by creating their online store. </p>
-            <p>At Mosganda, registered sellers can sell a wide range of products ranging from Electronics, Furniture, Fashion/clothes, down to even the market woman that sells Tomatoes. However, Mosganda does not accept listing of products that cannot be sold legally within the country.
+            <p>Mosganda is an online platform/marketplace for buying and selling goods and services. We give micro and small businesses the opportunity to bring their business online by creating their online store. We believe your business deserves to be online. With Mosganda, you do not need to bother about the cost of creating and maintaining a personal website for your business. Whatever you can sell legally in your physical store, you can also sell on Mosganda. However, Mosganda does not accept listing of products that cannot be sold legally within your state/country.
+                Mosganda connect buyers and sellers and give them the opportunity to communicate effectively through the Mosganda chat.
             </p>
-            <p>We connect buyers and sellers and give them the opportunity to communicate effectively through our chat.</p>
+            
             <h4>Our Vision</h4>
-            <p>To make life easy for buyers and sellers across the globe.
+            <p>To become a global online market.
             </p>
             <h4>Our Mission</h4>
-            <p>We seek to develop an effective, well placed online selling platform for individuals, micro and small enterprises.</p>
-            <div>
+            <p>We strive to build an effective, well-placed online marketplace for individuals, micro and small enterprises.</p>
+            
                 <h4>Objectives</h4>
-            <p>To provide an online platform as a marketplace for sellers and buyers.</p>
-            <p>To bring every small and medium business online.</p>
-            <p>To provide an online store for small businesses that could not afford the cost of shop rent.</p>
+            <p>To provide a convenient online trading platform for sellers and buyers.</p>
+            <p>To provide an online store for small businesses that could not afford the cost of building and maintaining a website for their business.</p>
             <p>To facilitate buying and selling within our towns/cities.</p>
+            <p>To provide a fun-filled shopping experience through effective communication and networking.</p>
             <p>To promote and facilitate trade in Nigeria, and across the globe.</p>
-            </div>
+            
             <h4>Our Core Values</h4>
             <p>Our core values are captured in the word <b>TIPS</b>.</p>
             <p><strong>Teamwork:</strong> We work together to meet the needs of our customers.</p>
